47f81775f69f82cc74b1e3322eb474b2cf42aac1
Maryam2506 <58718438+Maryam2506@users.noreply.github.com>
2020-07-15 20:34:46 +0500

    Completed

---
+++ src/pages/HomePage.js
@@ -0,0 +1,96 @@
+import React from "react";
+//Material ui/core used 
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography } from "@material-ui/core";
+
+//Other Component Added
+import CategoryCard from "../components/Home/CategoryCard";
+// react router v6
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 10,
+    backgroundImage: 'url("bg.jpg")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    // alignItems: "center",
+
+    [theme.breakpoints.down("sm")]: {
+      backgroundImage: 'url("bg_portrait.jpg")',
+    },
+  },
+
+  heading: {
+    fontWeight: "bold",
+    marginTop: theme.spacing(5),
+
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "3rem",
+    },
+  },
+
+  cardContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+}));
+
+const HomePage = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.root}>
+      <Container>
+        <Typography
+          variant="h3"
+          color="primary"
+          align="center"
+          className={classes.heading}
+        
+        >
+          FASHION FOR EVERY YOU 
+        </Typography>
+
+        <Typography
+          variant="body1"
+          color="primary"
+          align="center"
+          className={classes.heading}
+        >
+          <span style={{ color: "rgb(8, 75, 128)", fontSize: "22px" }}>
+            Shoes <span style={{ color: "#f4920f" }}>'R Us</span>
+          </span>{" "}
+          has been your companion in happiness, both big and small, for over 50
+          years. Our roots are firmly grounded in Pakistan and with its people.
+        </Typography>
+
+        <div className={classes.cardContainer}>
+          <CategoryCard
+            name="Men"
+            imageURL="https://cf.shopee.com.my/file/480604554577d333a256f522f0384016"
+            onClick={() => navigate("/men")}
+          />
+          <CategoryCard
+            name="Woman"
+            imageURL="https://cf.shopee.com.my/file/305c95e430189af85bea0ed8bb2069e0"
+            onClick={() => navigate("/women")}
+          />
+          <CategoryCard
+            name="Kids"
+            imageURL="https://cf.shopee.com.my/file/668abae14fbbab7a6ef371a1f5d63714"
+            onClick={() => navigate("/kids")}
+          />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/utils/social.js
@@ -0,0 +1,3 @@
+export const facebookURL = "https://www.facebook.com/maryam.tufail2506/";
+export const githubURL = "https://github.com/Maryam2506";
+export const linkedInURL = "https://www.linkedin.com/in/maryamtufail/";
